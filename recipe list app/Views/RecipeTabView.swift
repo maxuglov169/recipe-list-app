539e843b29b14c8d6608e2b162ab5ca9c4c7ffae
maxuglov169 <maxuglov169@gmail.com>
2021-11-22 21:21:50 +0300

    - Add RecipeTabView.swift as new starting View - Implement TabView

---
+++ recipe list app/Views/RecipeTabView.swift
@@ -0,0 +1,39 @@
+//
+//  RecipeTabView.swift
+//  recipe list app
+//
+//  Created by Maxim Ugolnik on 22.11.21.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        TabView {
+            Text("Featured view")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+            
+        }
+        
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
