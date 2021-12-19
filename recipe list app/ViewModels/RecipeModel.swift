2f2db21cb7bb518ae63e32bb8c35568a7b315006
maxuglov169 <maxuglov169@gmail.com>
2021-12-20 00:04:35 +0300

    - Import Rational class to calculate common divider to a project - Add function getPortion in RecipeModel class to calculate portion sizes - Add Picker element with available serving sizes - Modify ingredient list to display different serving sizes based on target serving

--- recipe list app/ViewModels/RecipeModel.swift
+++ recipe list app/ViewModels/RecipeModel.swift
@@ -17,4 +17,59 @@ class RecipeModel: ObservableObject {
         
     }
     
+    static func getPortion(ingredient:Ingredient, recipeServings:Int, targetServings:Int) -> String {
+        
+        var portion = ""
+        var nominator = ingredient.num ?? 1
+        var denominator = ingredient.denum ?? 1
+        var wholePortions = 0
+        
+        if ingredient.num != nil {
+            // Get a single serving size
+            denominator *= recipeServings
+            
+            // Get target portion
+            nominator *= targetServings
+            
+            // Reduce fraction by common denominator
+            let divisor = Rational.greatestCommonDivisor(nominator, denominator)
+            nominator /= divisor
+            denominator /= divisor
+            
+            // Get whole portions if nominator > denominator
+            if nominator >= denominator {
+                wholePortions = nominator / denominator
+                nominator %= denominator
+                portion += String(wholePortions)
+            }
+            
+            // Express remainder as a fraction
+            if nominator > 0 {
+                portion += wholePortions > 0 ? " " : ""
+                portion += "\(nominator)/\(denominator)"
+            }
+        }
+        
+        if var unit = ingredient.unit {
+            
+            if wholePortions > 1 {
+                //Calculate appropriate suffix
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                } else if unit.suffix(1) == "f" {
+                    unit = String(unit.dropLast())
+                    unit += "ves"
+                } else {
+                    unit += "s"
+                }
+            }
+            
+            portion += ingredient.num == nil && ingredient.denum == nil ? "" : " "
+            return portion + unit
+        }
+        
+        return portion
+        
+    }
+    
 }
