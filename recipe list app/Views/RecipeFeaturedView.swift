1c4c7bf23b5639893dc6bd410484f582151019b9
maxuglov169 <maxuglov169@gmail.com>
2021-12-19 19:28:40 +0300

    1. Build UI in RecipeFeaturedView

--- recipe list app/Views/RecipeFeaturedView.swift
+++ recipe list app/Views/RecipeFeaturedView.swift
@@ -12,12 +12,69 @@ struct RecipeFeaturedView: View {
     @EnvironmentObject var model:RecipeModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack (alignment: .leading, spacing: 0) {
+            
+            Text("Featured Recipes")
+                .bold()
+                .font(.largeTitle)
+                .padding(.leading)
+                .padding(.top, 40)
+                
+            GeometryReader { geo in
+                
+                TabView {
+                    ForEach (0..<model.recipes.count) { index in
+                        if model.recipes[index].featured {
+                            
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                    
+                                
+                                VStack {
+                                    Image(model.recipes[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+                                    Text(model.recipes[index].name)
+                                        .padding()
+                                }
+                                
+                            }
+                            .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
+                            .cornerRadius(15)
+                            .shadow(radius: 10)
+                            
+                            
+                        }
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+                
+            }
+            
+            VStack (alignment: .leading, spacing: 10) {
+                Text("Preparation Time:")
+                    .font(.headline)
+                Text("1 hour")
+                Text("Highlights")
+                    .font(.headline)
+                Text("Healthy, Hearty")
+            }
+            .padding([.leading, .bottom])
+            
+        }
+        
+        
+        
     }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeFeaturedView()
+            .environmentObject(RecipeModel())
     }
 }
