be40c288e8c6de88fa7037366de0999b9d9264b7
maxuglov169 <maxuglov169@gmail.com>
2021-12-19 20:19:51 +0300

    - change swiftui list to scrollview, lazyvstack and foreach

--- recipe list app/Views/RecipeListView.swift
+++ recipe list app/Views/RecipeListView.swift
@@ -13,33 +13,45 @@ struct RecipeListView: View {
     
     var body: some View {
         
-        NavigationView {
+        VStack(alignment: .leading) {
+            Text("All Recipes")
+                .bold()
+                .font(.largeTitle)
+                .padding(.top, 40)
             
-            List(model.recipes) { r in
-                
-                NavigationLink {
-                    RecipeDetailView(recipe: r)
-                } label: {
-                    HStack(spacing: 20.0) {
-                        Image(r.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 50, height: 50, alignment: .center)
-                            .clipped()
-                            .cornerRadius(10)
-                        Text(r.name)
-                            .foregroundColor(.gray)
-                    }
+            ScrollView {
+                LazyVStack(alignment: .leading) {
+                    ForEach(model.recipes) { r in
+                        
+                        NavigationLink {
+                            RecipeDetailView(recipe: r)
+                        } label: {
+                            HStack(spacing: 20.0) {
+                                Image(r.image)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 50, height: 50, alignment: .center)
+                                    .clipped()
+                                    .cornerRadius(10)
+                                Text(r.name)
+                                    .foregroundColor(.black)
+                            }
+                        }
+                        
+                    }.navigationBarHidden(true)
+                    
                 }
-
+                
             }
-            .navigationBarTitle("Recipes")
         }
+        .padding(.leading)
+        
     }
 }
 
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
